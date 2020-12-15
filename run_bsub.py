353cd430779da3c5d96a5a868a0e98af6c6fc5cf
braudelan <braudelan@gmail.com>
2020-12-15 14:48:00 +0200

    Tue 15 Dec 2020 14:50:13 IST
    - pass the job command as a command line argument
    - do job command pattern replacments in a for loop

--- run_bsub.py
+++ run_bsub.py
@@ -9,11 +9,8 @@ be provided by the user as arguments of the main script.
 
     Todo:
         * write the main loop as a function.
-        * provide a defualt -oe parmeter for bsub
         * make sure no email is sent after job completion if error and output are saved to file.
 """
-
-import os
 import re
 import subprocess
 import argparse
@@ -22,34 +19,48 @@ from datetime import datetime
 
 ENCODING = 'utf-8'
 
-# parse command line arguments
+# intialize command line parser
 parser = argparse.ArgumentParser()
 
-# argumetns for the job to be run
+# job arguments
+bsub_args = parser.add_argument_group()
 parser.add_argument('-in', '--input_dir',
                     help='directory to look for files to be processed')
 parser.add_argument('-out', '--output_dir',
                     help='directory where processed files are saved' )
-parser.add_argument('-regax',
-                    help='the regax expression to be substituted into the command')
-parser.add_argument('-c', '-command',
-                    help='the command to be executed by bsub')
+parser.add_argument('-regex',
+                    help='the match will be substituted into the command')
+parser.add_argument(
+    '-c',
+    '--command',
+    help='the command to be executed by bsub. '\
+         'a literal $file will be replaced by regex match, '\
+         'a literal $input_dir will be replaced by input_dir, '\
+         'a literal $output_dir will be replaced by output_dir.'
+)
 # bsub arguments
 parser.add_argument('-q', '--queue', type=str)
 parser.add_argument('-m', '--memory', type=int, help='requested memory in MB')
 
+# parse
 args = parser.parse_args()
 
-## bsub options
+# bsub options
 queue = ['-q', args.queue]
 required_memory = f'rusage[mem={args.memory}MB]'
 memory = ['-R', required_memory] if args.memory else None
 
-# job specific variables
+# job variables
 input_dir = args.input_dir
 output_dir = args.output_dir
-regax_expression = args.regax
-job_command = args.command
+file_id_regex = args.regex
+command = args.command
+
+# patterns to be replcaed in the job command
+file_pattern = r'\$file'
+input_dir_pattern = r'\$input_dir'
+output_dir_pattern = r'\$output_dir'
+
 
 # get file names
 get_file_names = subprocess.Popen([f'ls -1 {input_dir}'],
@@ -57,60 +68,60 @@ get_file_names = subprocess.Popen([f'ls -1 {input_dir}'],
 file_names = [byte.decode(ENCODING) for
               byte in get_file_names.stdout.read().splitlines()]
 
-## run a busb job for every file
-reg_expression = r'^\w+_\w+_\w+_R'
-reg_compile = re.compile(reg_expression)
+## run a bsub job for each file in input dir
+compiled_pattern = re.compile(rf'{file_id_regex}')
 for file_name in file_names:
 
-    ## match the regax expression
-    match = reg_compile.match(file_name)
-    unique_sample_id = match.group()
-
+    ## match pattern and return match
+    match = compiled_pattern.match(file_name)
+    file_id = match.group()
+    # print(f'file_id: {file_id} \n\n')
     # generate a job hash
     now = datetime.now()
-    job_and_time = f'{unique_sample_id}.{now}'.encode()
+    job_and_time = f'{file_id}.{now}'.encode()
     job_hash = hashlib.md5(job_and_time).hexdigest()
 
-    # job file name
+    # job name
     job_id = f'job.{job_hash}'
 
-
-    # build the command for the specific tool in use\
-    # using the unique sample ID
-    analysis_tool_command = f'cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A '\
-                            f'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT --times 2 -q 30 '\
-                            f'-m 20 -o 02_trimmed_reads/{unique_sample_id}1_trimmed.fastq '\
-                            f'-p 02_trimmed_reads/{unique_sample_id}2_trimmed.fastq '\
-                            f'01.fastq/{unique_sample_id}1_001.fastq.gz '\
-                            f'01.fastq/{unique_sample_id}2_001.fastq.gz/'
-    analysis_tool_command = f"'{analysis_tool_command}'"
+    # insert relevant input and output dir and file name into command
+    pattern_replacement = [
+        (file_pattern, file_id),
+        (input_dir_pattern, input_dir),
+        (output_dir_pattern, output_dir)
+    ]
+    job_command = command
+    for pattern, replacement in pattern_replacement:
+        job_command = re.sub(pattern, replacement, job_command)
+    job_command = f'\'{job_command}\''
 
     # build the paths and commands for bsub's output\
     # and error files
-    error_file_command = ['-e', f'err/{job_id}']
-    output_file_command = ['-o',f'out/{job_id}']
+    bsub_error_path = ['-e', f'err/{job_id}']
+    bsub_output_path = ['-o', f'out/{job_id}']
 
-    commands = [
+    options_for_command = [
         'bsub',
         queue,
         memory,
-    #     error_file_command,
-    #    output_file_command,
-        analysis_tool_command,
+        bsub_error_path,
+        bsub_output_path,
+        job_command,
     ]
 
     commands_to_run = []
-    for command in commands:
-        if command and type(command) != list:
-            commands_to_run.append(command)
-        elif command and type(command) == list:
-            commands_to_run.extend(command)
+    for variable in options_for_command:
+        if variable and type(variable) != list:
+            commands_to_run.append(variable)
+        elif variable and type(variable) == list:
+            commands_to_run.extend(variable)
         else:
             continue
 
     # run the complete command in bash		
-    # command_to_run = " ".join(commands_to_run)
-    # subprocess.run(command_to_run, text=True, shell=True)
+    command_to_run = " ".join(commands_to_run)
+    print(command_to_run + '\n\n')
+    # subprocess.run(command_to_run, text=True, shell=True) # uncomment this when running in WEXAC
 
 
 
